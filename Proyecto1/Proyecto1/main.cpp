a450092dcf5d0671746cb3032d305af7afda6b2f
C4risrocks <christiandejmorenomarin@MacBook-Pro-de-Christian.local>
2015-10-13 20:05:04 -0500

    firstCommit
    xD

---
+++ Proyecto1/Proyecto1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proyecto1
+//
+//  Created by Christian de J Moreno Marín on 13/10/15.
+//  Copyright © 2015 Christian de J Moreno Marín. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
